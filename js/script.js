5258c12b4c8a1b0efc4b27e564ecea0b6a5070a7
Fernando <ferg.developer@gmail.com>
2021-11-29 18:56:22 -0500

    adding transitions and animations to website

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
 //Navbar
 
+const mobileBtn = document.getElementById('mobile-cta')
+nav = document.querySelector('nav');
+mobileBtnExit = document.getElementById('mobile-exit');
+
+mobileBtn.addEventListener('click', () => {
+    nav.classList.add('menu-btn');
+})
+
+mobileBtnExit.addEventListener('click', () => {
+    nav.classList.remove('menu-btn');
+})
+
+
 // Footer Current Year
 var date = new Date(); 
 var year = date.getFullYear();
